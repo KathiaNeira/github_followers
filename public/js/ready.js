0d1315241abc46ff8f569c60f1552cebec8cd162
kathia <kathia.neira@orbis.com.pe>
2016-09-27 14:55:07 -0500

    feat(followers):actualizando js

--- public/js/ready.js
+++ public/js/ready.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 			follow	: '#follow',
 			modal	 : '.overlay',
 			repos  	: '.repos',
+			reposFollowers : '.reposFollowers',
 			nameFollowers : ''
 		};
 		catchDom = function(){
@@ -14,6 +15,7 @@ $(document).ready(function(){
 			dom.follow	= $(st.follow);
 			dom.modal	 = $(st.modal);
 			dom.repos   = $(st.repos);
+			dom.reposFollowers = $(st.reposFollowers);
 		};
 		afterCatchDom = function(){
 			functions.send();
@@ -26,17 +28,20 @@ $(document).ready(function(){
 			showModal: function(){
 				console.log("Enviaste un regalo =)");
 				$(st.modal).css("display", "flex");
-				functions.repos(st.nameFollowers);
 			},
 			change: function(){
 				if ($(this).val() != 0) {
 					st.nameFollowers = $(this).val();
 					//$(st.modal).empty();
-					$(st.modal).css("display", "none")
+					$(st.modal).css("display", "flex")
 					$(st.btnSend).removeAttr('disabled');
 					$(st.btnSend).css("background", "#7B7BDA");
+					functions.repos(st.nameFollowers);
 					// $(st.repos).append(`<span> ` + $(this).val()+ `</span>`  );
 				}else{
+					$(st.modal).css("display", "none")
+					$(st.reposFollowers).empty();
+					$(st.repos).empty();
 					console.log("no hay nada");
 					$(st.btnSend).attr("disabled", "disabled");
 					$(st.btnSend).css("background", "#CBCBF1");
@@ -55,20 +60,23 @@ $(document).ready(function(){
 							var height = 200;
 							$("select").append('<option>' +data.login+ '</option>');
 							console.log(data.avatar_url);
-							$(st.repos).append('<img src=" '+data.avatar_url+' " class="img_repo"/>'); //imagen
 					});
 					}
 				});
 			},
 			repos: function(nameFollowers){
+				console.log('nameFollowers', nameFollowers)
+				$(st.reposFollowers).empty();
+				$(st.repos).empty();
 				$.ajax({
 					url: "https://api.github.com/users/"+nameFollowers+"/repos",
 					type: "GET",
 					dataType: "json",
 					success:function(respuesta){
+						console.log('respuesta',respuesta)
 						respuesta.forEach(function(data){
-							console.log(data);
-							$(st.modal).append('<a href="">' +data.name+ '</a>')
+							$(st.reposFollowers).append('<div class="projects"><a href="">' +data.name+ '</a></div>')
+							$(st.repos).append('<img src=" '+data.owner.avatar_url+' " class="img_repo"/>'); //imagen
 						});
 					}
 				});
